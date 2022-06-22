3f590afa8b19efd3134de1e1ed4b327c76e7ae5f
jovianka <jovianpratama123@gmail.com>
2022-06-22 14:27:40 +0700

    implemented useState and useEffect hooks on react-router NavLink

--- client/src/components/NavigationBar.tsx
+++ client/src/components/NavigationBar.tsx
@@ -1,11 +1,32 @@
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
-import NavDropdown from 'react-bootstrap/NavDropdown';
+import { NavLink } from 'react-router-dom';
+import { useState, useEffect } from 'react';
 
 export default function NavigationBar() {
+
+  const [navLinkState, setNavLinkState] = useState("");
+
+  useEffect(() => {
+    setNavLinkState(`${({ isActive }: {isActive: any}) => isActive ? "active" : ""}`);
+  }, [navLinkState])
+  
+
   return (
     <Navbar bg="dark" variant="dark" expand="lg" >
+      <Container>
+        <Navbar.Brand href={process.env.PUBLIC_URL + "/"}>
+          <img src="mesl-icon.png" alt="MESL Logo" width={48} height={48} />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link as={ NavLink } to="/registration" className={ navLinkState }>Registration</Nav.Link>
+            <Nav.Link href="#link">Link</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
     </Navbar>
   )
 }
