e41c6a04251d09c726478e0707f58e2f688c141b
jovianka <jovianpratama123@gmail.com>
2022-06-22 13:34:50 +0700

    some tidying up and added more images

--- client/src/components/NavigationBar.tsx
+++ client/src/components/NavigationBar.tsx
@@ -1,9 +1,11 @@
+import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 
 export default function NavigationBar() {
   return (
-    <Navbar>
-
-    </Navbar>  
+    <Navbar bg="dark" variant="dark" expand="lg" >
+    </Navbar>
   )
 }
