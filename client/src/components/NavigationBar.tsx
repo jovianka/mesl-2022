8a57d9ee5bca63798d5d4810cad4db3e7c1f4a1b
jovianka <jovianpratama123@gmail.com>
2022-06-23 12:34:21 +0700

    changed images to .webp for better compression, changed homepage public url to firebase app

--- client/src/components/NavigationBar.tsx
+++ client/src/components/NavigationBar.tsx
@@ -2,7 +2,7 @@ import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 import Button from 'react-bootstrap/Button';
-import MESLIcon from '../images/mesl-icon.png';
+import MESLIcon from '../images/mesl-icon.webp';
 import { NavLink } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 
