14ebc0d5f7f1b02acd7a0b1503dd086b4197c1bb
jovianka <jovianpratama123@gmail.com>
2022-06-27 03:44:15 +0700

    added footer

---
+++ client/src/components/Footer.tsx
@@ -0,0 +1,8 @@
+export default function Footer() {
+  return ( 
+    <footer className="border-top py-4 text-center text-muted">
+      Copyright &copy; 2022 Jovianka Pratama
+    </footer>
+
+   );
+}
