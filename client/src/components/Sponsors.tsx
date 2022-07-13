792080570338d0456c5f3dfd49cd7359c40494c4
jovianka <jovianpratama123@gmail.com>
2022-07-14 01:17:37 +0700

    updated spacing and sponsors layout

--- client/src/components/Sponsors.tsx
+++ client/src/components/Sponsors.tsx
@@ -7,23 +7,28 @@ import google from '../images/google.png';
 
 function Sponsors() {
   return ( 
-    <div className="Sponsors py-5">
+    <div className="Sponsors py-5 my-5">
       <h1 className="text-center pb-3">Sponsors</h1>
       <Container>
-        <Row className="gy-4 row-cols-2 row-cols-md-3" data-aos="fade-up">
-          <Col md="4" className="justify-content-center sponsors-container">
-            <a href="#daftarValorant" className="sponsors-link">
+        <Row className="g-3 row-cols-2 row-cols-md-4" data-aos="fade-up">
+          <Col className="justify-content-center sponsors-container">
+            <a href="/registrasi" className="sponsors-link">
               <img src={google} alt="Google Logo" className="sponsors-image" />
             </a>
           </Col>
-          <Col md="4" className="justify-content-center sponsors-container">
-            <a href="#daftarValorant" className="sponsors-link">
+          <Col className="justify-content-center sponsors-container">
+            <a href="/registrasi" className="sponsors-link">
               <img src={googleLogo} alt="Google Logo" className="sponsors-image" />
             </a>
           </Col>
-          <Col md="4" className="justify-content-center sponsors-container">
-            <a href="#daftarValorant" className="sponsors-link">
+          <Col className="justify-content-center sponsors-container">
+            <a href="/registrasi" className="sponsors-link">
               <img src={google} alt="Google Logo" className="sponsors-image" />
+            </a>
+          </Col>
+          <Col className="justify-content-center sponsors-container">
+            <a href="/registrasi" className="sponsors-link">
+              <img src={googleLogo} alt="Google Logo" className="sponsors-image" />
             </a>
           </Col>
         </Row>
