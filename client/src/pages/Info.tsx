aa81535f8c72d10e66fd97deae8ca2f8934227e3
jovianka <jovianpratama123@gmail.com>
2022-07-19 22:49:15 +0700

    moved peraturan-peraturan lomba to info page

--- client/src/pages/Info.tsx
+++ client/src/pages/Info.tsx
@@ -1,7 +1,120 @@
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+import Collapse from 'react-bootstrap/Collapse';
+
+import { useState } from 'react';
+
+import mlbbLogo from '../images/mlbb-logo.webp';
+import valorantLogo from '../images/valorant-logo.svg';
+import pubgmLogo from '../images/pubgm-logo.webp';
+
 export default function Info() {
+
+  const [collapseAturanValorant, setOpenCollapseAturanValorant] = useState(true);
+  const [collapseAturanMLBB, setOpenCollapseAturanMLBB] = useState(true);
+  const [collapseAturanPUBGM, setOpenCollapseAturanPUBGM] = useState(true);
+
   return (
-    <div className="Info text-center">
-      <h1>COMING SOON!</h1>
+    <div className="Info">
+      <Container>
+        <div id="aturanValorant">
+          <Row className="justify-content-center my-5">
+            <Col sm="10">
+              <Card bg="secondary" className="p-3">
+                <Button variant="secondary" className="w-100" onClick={() => setOpenCollapseAturanValorant(!collapseAturanValorant)}>
+                  <Card.Header className="border-0 d-flex justify-content-center align-items-center">
+                    <h2 className="text-center bg-transparent">Peraturan Valorant</h2>
+                      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="white" className="bi bi-caret-down ms-2" viewBox="0 0 16 16">
+                        <path d="M3.204 5h9.592L8 10.481 3.204 5zm-.753.659 4.796 5.48a1 1 0 0 0 1.506 0l4.796-5.48c.566-.647.106-1.659-.753-1.659H3.204a1 1 0 0 0-.753 1.659z"/>
+                      </svg>
+                  </Card.Header>
+                </Button>
+                <Collapse in={collapseAturanValorant}>
+                  <div id="collapseAturanValorant">
+                    <Card.Body className="border-top">
+                      <ol>
+                        <li>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error asperiores animi qui id beatae possimus architecto, odio sapiente, quasi tenetur consectetur alias illo. Non voluptates corporis pariatur, odio odit placeat.</li>
+                        <li>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error asperiores animi qui id beatae possimus architecto, odio sapiente, quasi tenetur consectetur alias illo. Non voluptates corporis pariatur, odio odit placeat.</li>
+                        <li>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error asperiores animi qui id beatae possimus architecto, odio sapiente, quasi tenetur consectetur alias illo. Non voluptates corporis pariatur, odio odit placeat.</li>
+                        <li>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error asperiores animi qui id beatae possimus architecto, odio sapiente, quasi tenetur consectetur alias illo. Non voluptates corporis pariatur, odio odit placeat.</li>
+                        <li>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error asperiores animi qui id beatae possimus architecto, odio sapiente, quasi tenetur consectetur alias illo. Non voluptates corporis pariatur, odio odit placeat.</li>
+                      </ol>
+                    </Card.Body>
+                  </div>
+                </Collapse>
+              </Card>
+            </Col>
+          </Row>
+        </div>
+        <hr/>
+        <div id="aturanMLBB">
+          <Row className="justify-content-center my-5">
+            <Col sm="10">
+              <Card bg="secondary" className="p-3">
+                <Button variant="secondary" className="w-100" onClick={() => setOpenCollapseAturanMLBB(!collapseAturanMLBB)}>
+                  <Card.Header className="border-0 d-flex justify-content-center align-items-center">
+                    <h2 className="text-center bg-transparent">Peraturan MLBB</h2>
+                      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="white" className="bi bi-caret-down ms-2" viewBox="0 0 16 16">
+                        <path d="M3.204 5h9.592L8 10.481 3.204 5zm-.753.659 4.796 5.48a1 1 0 0 0 1.506 0l4.796-5.48c.566-.647.106-1.659-.753-1.659H3.204a1 1 0 0 0-.753 1.659z"/>
+                      </svg>
+                  </Card.Header>
+                </Button>
+                <Collapse in={collapseAturanMLBB}>
+                  <div id="collapseAturanMLBB">
+                    <Card.Body className="border-top">
+                      <ol>
+                        <li>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error asperiores animi qui id beatae possimus architecto, odio sapiente, quasi tenetur consectetur alias illo. Non voluptates corporis pariatur, odio odit placeat.</li>
+                        <li>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error asperiores animi qui id beatae possimus architecto, odio sapiente, quasi tenetur consectetur alias illo. Non voluptates corporis pariatur, odio odit placeat.</li>
+                        <li>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error asperiores animi qui id beatae possimus architecto, odio sapiente, quasi tenetur consectetur alias illo. Non voluptates corporis pariatur, odio odit placeat.</li>
+                        <li>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error asperiores animi qui id beatae possimus architecto, odio sapiente, quasi tenetur consectetur alias illo. Non voluptates corporis pariatur, odio odit placeat.</li>
+                        <li>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error asperiores animi qui id beatae possimus architecto, odio sapiente, quasi tenetur consectetur alias illo. Non voluptates corporis pariatur, odio odit placeat.</li>
+                      </ol>
+                    </Card.Body>
+                  </div>
+                </Collapse>
+              </Card>
+            </Col>
+          </Row>
+        </div>
+
+        <hr />
+
+        <div id="aturanPUBGM">
+          <Row className="justify-content-center my-5">
+            <Col sm="10">
+              <Card bg="secondary" className="p-3">
+                <Button variant="secondary" className="w-100" onClick={() => setOpenCollapseAturanPUBGM(!collapseAturanPUBGM)}>
+                  <Card.Header className="border-0 d-flex justify-content-center align-items-center">
+                    <h2 className="text-center bg-transparent">Peraturan PUBGM</h2>
+                      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="white" className="bi bi-caret-down ms-2" viewBox="0 0 16 16">
+                        <path d="M3.204 5h9.592L8 10.481 3.204 5zm-.753.659 4.796 5.48a1 1 0 0 0 1.506 0l4.796-5.48c.566-.647.106-1.659-.753-1.659H3.204a1 1 0 0 0-.753 1.659z"/>
+                      </svg>
+                  </Card.Header>
+                </Button>
+                <Collapse in={collapseAturanPUBGM}>
+                  <div id="collapseAturanPUBGM">
+                    <Card.Body className="border-top">
+                      <ol>
+                        <li>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error asperiores animi qui id beatae possimus architecto, odio sapiente, quasi tenetur consectetur alias illo. Non voluptates corporis pariatur, odio odit placeat.</li>
+                        <li>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error asperiores animi qui id beatae possimus architecto, odio sapiente, quasi tenetur consectetur alias illo. Non voluptates corporis pariatur, odio odit placeat.</li>
+                        <li>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error asperiores animi qui id beatae possimus architecto, odio sapiente, quasi tenetur consectetur alias illo. Non voluptates corporis pariatur, odio odit placeat.</li>
+                        <li>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error asperiores animi qui id beatae possimus architecto, odio sapiente, quasi tenetur consectetur alias illo. Non voluptates corporis pariatur, odio odit placeat.</li>
+                        <li>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error asperiores animi qui id beatae possimus architecto, odio sapiente, quasi tenetur consectetur alias illo. Non voluptates corporis pariatur, odio odit placeat.</li>
+                      </ol>
+                    </Card.Body>
+                  </div>
+                </Collapse>
+              </Card>
+            </Col>
+          </Row>
+        </div>
+
+        <hr />
+        
+      </Container>
     </div> 
   )
 }
--- client/src/pages/Registrasi.tsx
+++ client/src/pages/Registrasi.tsx
@@ -10,6 +10,8 @@ import Collapse from 'react-bootstrap/Collapse';
 
 import { useState } from 'react';
 
+import { Link } from 'react-router-dom';
+
 import mlbbLogo from '../images/mlbb-logo.webp';
 import valorantLogo from '../images/valorant-logo.svg';
 import pubgmLogo from '../images/pubgm-logo.webp';
@@ -21,9 +23,6 @@ import pubgLogo from '../images/pubg-logo.webp';
 export default function Registrasi() {
 
   const [collapseAturanPendaftaran, setOpenCollapseAturanPendaftaran] = useState(true);
-  const [collapseAturanValorant, setOpenCollapseAturanValorant] = useState(false);
-  const [collapseAturanMLBB, setOpenCollapseAturanMLBB] = useState(false);
-  const [collapseAturanPUBGM, setOpenCollapseAturanPUBGM] = useState(false);
 
   return (
     <main className="Registrasi">
@@ -61,8 +60,8 @@ export default function Registrasi() {
         </section>
 
 
-        <section className="lomba my-5 pb-4">
-          {/* GAME BUTTONS */}
+        {/* GAME BUTTONS */}
+        <section className="lomba mt-5">
           <h1 className="text-center mb-5">Games</h1>
           <Row className="row-cols-2 row-cols-sm-3 gy-3 justify-content-center">
             <Col lg="3">
@@ -86,7 +85,7 @@ export default function Registrasi() {
           {/* FORM BUTTONS */}
           <div className="formulir-pendaftaran">
             
-            <div id="daftarValorant" className="mt-5">
+            <div id="daftarValorant" className="my-5">
               <Row className="justify-content-center align-items-center">
                 <Col md="6" className="d-flex justify-content-center">
                   <AnimationOnScroll initiallyVisible={true} animateIn="animate__pulse">
@@ -98,39 +97,15 @@ export default function Registrasi() {
                   <p>(Match Description)Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sapiente, consequatur! Nesciunt reprehenderit fuga nihil nisi molestias fugit animi recusandae ea.</p>
                   <p className="fs-5 fst-italic">Biaya Pendaftaran: Rp120.000</p>
                   <Button variant="accent" size="lg" as="a" href="">Daftar Lomba Valorant</Button>
-                </Col>
-              </Row>
-              <Row className="justify-content-center mt-4">
-                <Col sm="12">
-                  <Card bg="secondary" className="p-3">
-                    <Button variant="secondary" className="w-100" onClick={() => setOpenCollapseAturanValorant(!collapseAturanValorant)}>
-                      <Card.Header className="border-0 d-flex justify-content-center align-items-center">
-                        <h2 className="text-center">Aturan Valorant</h2>
-                          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="white" className="bi bi-caret-down ms-2" viewBox="0 0 16 16">
-                            <path d="M3.204 5h9.592L8 10.481 3.204 5zm-.753.659 4.796 5.48a1 1 0 0 0 1.506 0l4.796-5.48c.566-.647.106-1.659-.753-1.659H3.204a1 1 0 0 0-.753 1.659z"/>
-                          </svg>
-                      </Card.Header>
-                    </Button>
-                    <Collapse in={collapseAturanValorant}>
-                      <div id="collapseAturanValorant">
-                        <Card.Body className="border-top">
-                          <ol>
-                            <li>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error asperiores animi qui id beatae possimus architecto, odio sapiente, quasi tenetur consectetur alias illo. Non voluptates corporis pariatur, odio odit placeat.</li>
-                            <li>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error asperiores animi qui id beatae possimus architecto, odio sapiente, quasi tenetur consectetur alias illo. Non voluptates corporis pariatur, odio odit placeat.</li>
-                            <li>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error asperiores animi qui id beatae possimus architecto, odio sapiente, quasi tenetur consectetur alias illo. Non voluptates corporis pariatur, odio odit placeat.</li>
-                            <li>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error asperiores animi qui id beatae possimus architecto, odio sapiente, quasi tenetur consectetur alias illo. Non voluptates corporis pariatur, odio odit placeat.</li>
-                            <li>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error asperiores animi qui id beatae possimus architecto, odio sapiente, quasi tenetur consectetur alias illo. Non voluptates corporis pariatur, odio odit placeat.</li>
-                          </ol>
-                        </Card.Body>
-                      </div>
-                    </Collapse>
-                  </Card>
+                  <br />
+                  <Link to="/info#aturanValorant"><Button variant="outline-accent text-light mt-3" size="lg">Peraturan</Button></Link>
                 </Col>
               </Row>
             </div>
 
+            <hr />
 
-            <div id="daftarMLBB" className="mt-5">
+            <div id="daftarMLBB" className="my-5">
               <Row className="justify-content-center align-items-center">
                 <Col md="6" className="">
                   <AnimationOnScroll initiallyVisible={true} animateIn="animate__pulse">
@@ -140,41 +115,17 @@ export default function Registrasi() {
                 <Col md="6">
                   <h2>Mobile Legends: Bang Bang</h2>
                   <p>(Match Description)Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sapiente, consequatur! Nesciunt reprehenderit fuga nihil nisi molestias fugit animi recusandae ea.</p>
-                  <p className="fs-5 fst-italic">Biaya Pendaftaran: Rp70.000</p>
+                  <p className="fs-5 fst-italic ">Biaya Pendaftaran: Rp70.000</p>
                   <Button variant="accent" size="lg" as="a" href="">Daftar Lomba MLBB</Button>
-                </Col>
-              </Row>
-              <Row className="justify-content-center mt-4">
-                <Col sm="12">
-                  <Card bg="secondary" className="p-3">
-                    <Button variant="secondary" className="w-100" onClick={() => setOpenCollapseAturanMLBB(!collapseAturanMLBB)}>
-                      <Card.Header className="border-0 d-flex justify-content-center align-items-center">
-                        <h2 className="text-center">Aturan MLBB</h2>
-                          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="white" className="bi bi-caret-down ms-2" viewBox="0 0 16 16">
-                            <path d="M3.204 5h9.592L8 10.481 3.204 5zm-.753.659 4.796 5.48a1 1 0 0 0 1.506 0l4.796-5.48c.566-.647.106-1.659-.753-1.659H3.204a1 1 0 0 0-.753 1.659z"/>
-                          </svg>
-                      </Card.Header>
-                    </Button>
-                    <Collapse in={collapseAturanMLBB}>
-                      <div id="collapseAturanMLBB">
-                        <Card.Body className="border-top">
-                          <ol>
-                            <li>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error asperiores animi qui id beatae possimus architecto, odio sapiente, quasi tenetur consectetur alias illo. Non voluptates corporis pariatur, odio odit placeat.</li>
-                            <li>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error asperiores animi qui id beatae possimus architecto, odio sapiente, quasi tenetur consectetur alias illo. Non voluptates corporis pariatur, odio odit placeat.</li>
-                            <li>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error asperiores animi qui id beatae possimus architecto, odio sapiente, quasi tenetur consectetur alias illo. Non voluptates corporis pariatur, odio odit placeat.</li>
-                            <li>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error asperiores animi qui id beatae possimus architecto, odio sapiente, quasi tenetur consectetur alias illo. Non voluptates corporis pariatur, odio odit placeat.</li>
-                            <li>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error asperiores animi qui id beatae possimus architecto, odio sapiente, quasi tenetur consectetur alias illo. Non voluptates corporis pariatur, odio odit placeat.</li>
-                          </ol>
-                        </Card.Body>
-                      </div>
-                    </Collapse>
-                  </Card>
+                  <br />
+                  <Link to="/info#aturanMLBB"><Button variant="outline-accent text-light mt-3" size="lg">Peraturan</Button></Link>
                 </Col>
               </Row>
             </div>
 
+            <hr />
 
-            <div id="daftarPUBGM" className="mt-5">
+            <div id="daftarPUBGM" className="my-5">
               <Row className="justify-content-center align-items-center">
                 <Col md="6" className="">
                   <AnimationOnScroll initiallyVisible={true} animateIn="animate__pulse">
@@ -185,38 +136,14 @@ export default function Registrasi() {
                   <h2>PUBG Mobile</h2>
                   <p>(Match Description)Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sapiente, consequatur! Nesciunt reprehenderit fuga nihil nisi molestias fugit animi recusandae ea.</p>
                   <p className="fs-5 fst-italic">Biaya Pendaftaran: Rp70.000</p>
-                  <Button variant="accent" size="lg" as="a" href="">Daftar Lomba PUBG Mobile</Button>
-                </Col>
-              </Row>
-              <Row className="justify-content-center mt-4">
-                <Col sm="12">
-                  <Card bg="secondary" className="p-3">
-                    <Button variant="secondary" className="w-100" onClick={() => setOpenCollapseAturanPUBGM(!collapseAturanPUBGM)}>
-                      <Card.Header className="border-0 d-flex justify-content-center align-items-center">
-                        <h2 className="text-center">Aturan PUBGM</h2>
-                          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="white" className="bi bi-caret-down ms-2" viewBox="0 0 16 16">
-                            <path d="M3.204 5h9.592L8 10.481 3.204 5zm-.753.659 4.796 5.48a1 1 0 0 0 1.506 0l4.796-5.48c.566-.647.106-1.659-.753-1.659H3.204a1 1 0 0 0-.753 1.659z"/>
-                          </svg>
-                      </Card.Header>
-                    </Button>
-                    <Collapse in={collapseAturanPUBGM}>
-                      <div id="collapseAturanPUBGM">
-                        <Card.Body className="border-top">
-                          <ol>
-                            <li>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error asperiores animi qui id beatae possimus architecto, odio sapiente, quasi tenetur consectetur alias illo. Non voluptates corporis pariatur, odio odit placeat.</li>
-                            <li>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error asperiores animi qui id beatae possimus architecto, odio sapiente, quasi tenetur consectetur alias illo. Non voluptates corporis pariatur, odio odit placeat.</li>
-                            <li>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error asperiores animi qui id beatae possimus architecto, odio sapiente, quasi tenetur consectetur alias illo. Non voluptates corporis pariatur, odio odit placeat.</li>
-                            <li>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error asperiores animi qui id beatae possimus architecto, odio sapiente, quasi tenetur consectetur alias illo. Non voluptates corporis pariatur, odio odit placeat.</li>
-                            <li>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error asperiores animi qui id beatae possimus architecto, odio sapiente, quasi tenetur consectetur alias illo. Non voluptates corporis pariatur, odio odit placeat.</li>
-                          </ol>
-                        </Card.Body>
-                      </div>
-                    </Collapse>
-                  </Card>
+                  <Button variant="accent" size="lg" as="a" href="">Daftar Lomba PUBGM</Button>
+                  <br />
+                  <Link to="/info#aturanPUBGM"><Button variant="outline-accent text-light mt-3" size="lg">Peraturan</Button></Link>
                 </Col>
               </Row>
             </div>
 
+            <hr />
 
           </div>
         </section>
