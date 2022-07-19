eb3cd34210c6345d2f67e6f5d3fdbf9c63343b03
jovianka <jovianpratama123@gmail.com>
2022-07-19 19:07:49 +0700

    temporary fix for react-router scrolling issues

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,17 +5,21 @@ import Info from './pages/Info';
 import NavigationBar from './components/NavigationBar';
 import Sponsors from './components/Sponsors';
 import Footer from './components/Footer';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, useLocation } from 'react-router-dom';
 import './scss/App.scss';
 import 'animate.css/animate.min.css';
-import { useLocation } from "react-router-dom";
 
 function ScrollToTop() {
-  const { pathname } = useLocation();
+  const {pathname, hash} = useLocation();
 
   useEffect(() => {
-    setTimeout(() => { window.scrollTo(0, 0) }, 0);
-  }, [pathname]);
+    console.log(pathname);
+    if (!hash) {
+      setTimeout(() => window.scrollTo(0, 0), 0);
+    } else {
+      document.querySelector(hash)?.scrollIntoView();
+    }
+  }, [pathname, hash]);
 
   return null;
 }
